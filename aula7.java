b1f290905c8ed9c21783bb1cc8790d2f3779b932
rubengaspar <46534187+rubengaspar@users.noreply.github.com>
2021-11-17 22:18:29 +0000

    Update aula7.java

--- aula7.java
+++ aula7.java
@@ -1,6 +1,6 @@
 // Exercicios Color
 
-class aula7 {
+class Aula7 {
  	
   // a) Construir uma cor aleatória
   static Color randomColor() {
@@ -115,8 +115,8 @@ class aula7 {
 	
 	static void paintSquare (BinaryImage img, int x, int y, int side) {
 		
-		for (int w = x; w == (x+side); w++)
-			for (int h = y; h == (y+side); h++)
+		for (int w = x; w <= (x+side); w++)
+			for (int h = y; h <= (y+side); h++)
 				img.setWhite(w, h);
 		
 	}
@@ -125,58 +125,141 @@ class aula7 {
 	// c) Desenhar um contorno a preto nos píxeis limite da imagem
 	static void paintSquareWithBorder (BinaryImage img, int x, int y, int side) {
 		
-		for (int w = x; w == (x+side); w++)
-			for (int h = y; h == (y+side); h++)
+		for (int w = x; w <= (x+side); w++) {
+			for (int h = y; h <= (x+side); h++) {
 				if (w == x || w == (x+side) || h == y || h == (y+side))
 					img.setBlack(w, h);
 				else
 					img.setWhite(w, h);
-				
+			}
+		}
 	}
 
 	static void testPaintSquare () {
-		BinaryImage img = randomImage(200, 200);
-		paintSquareWithBorder(img, 40 , 40, 40);
+		BinaryImage img = randomImage(200, 400);
+		paintSquare(img, 60 , 60, 40);
+		return;
 	}
 	
 	// d) Criar uma imagem binária que forme um tabuleiro de xadrez, indicando o número de pixels de cada posição. 
 	//Note que há uma linha de um píxel em torno do tabuleiro. Pode utilizar a função paintSquare(...) desenvolvida anteriormente.
 	// BinaryImage board = createChessBoard(15);
-	
-	
-	
-	
-	
+	static BinaryImage createChessBoard(int a) {
+		
+		BinaryImage chess = new BinaryImage(a*8, a*8);
+		
+
+		for (int x = 0; x < 8; x++) {
+			for (int y = 0; y < 8; y++) {
+				if ((y % 2 == 0) == (x % 2 == 0)) 
+					paintSquare(chess, (a*x), (a*y), a-1);
+			}
+		}
+		return chess;
+	}
+			
 	// e) Inverter uma imagem binária, de forma a que cada pixel branco passe a preto, e vice-versa. 
 	// Desenvolva duas versões, um procedimento (que transforma a imagem) e uma função 
 	// (que cria uma imagem nova e deixa a existente inalterada).
+	static void invertImage (BinaryImage img) {
+		
+		for (int w = 0; w != img.getWidth(); w++) 
+			for (int h = 0; h != img.getHeight(); h++)
+				if (img.isBlack(w,h))
+					img.setWhite(w,h);
+				else
+					img.setBlack(w,h);		
+		
+	}
 	
-	
-	
+	static void testInvert () {
+		BinaryImage img = createChessBoard(20);
+		
+		System.out.println("Is the first square Black? " + img.isBlack(0,0));
+		
+		invertImage(img);
+		
+		System.out.println("Image Inverted");
+		System.out.println("Is the first square White? " + !img.isBlack(0,0));
+	}
 	
 	
 	// f) Criar uma imagem escalada por um fator (p.e., fator 2 duplica o tamanho da imagem).
 	// BinaryImage chess = createChessBoard(15);
 	// BinaryImage chessBig = scale(chess, 2);
 	
+	static BinaryImage scale (BinaryImage img, int a) {
+		
+		BinaryImage scale = new BinaryImage(img.getWidth()*a,img.getHeight()*a);
+		
+		for (int x = 0; x < img.getHeight(); x++) {
+			for (int y = 0; y < img.getWidth(); y++) {
+				if (img.isBlack(x,y)) {
+					scale.setBlack(x,y);
+					
+				} else {
+					scale.setWhite(x,y);
+					
+				}
+			}
+		}
+		return scale;
+	}
 	
-	
+	static void testScale () {
+	}
 	
 	//	g) Criar uma imagem binária através da sobreposição de duas imagens binárias, sendo que: 
 	//		se um pixel é branco em ambas as imagens, será branco na nova imagem; 
 	//		se um pixel é preto numa das imagens, será preto na nova imagem.
 	//		BinaryImage merge = merge(chess, chessBig);
 	
+	static BinaryImage merge(BinaryImage img1, BinaryImage img2) {
+		
+		int w, h;
+		///////////////////////////////////////////
+		//mudar para colar o a imagem mais pequena a ser colada na maior
+		//////////////////////////////////////////
+		w = Math.min(img1.getWidth(),img2.getWidth());
+		h = Math.min(img1.getHeight(),img2.getHeight());
+		
+		
+		BinaryImage merge = new BinaryImage(w,h);
+		
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				if ((img1.isBlack(x,y) == false) && (img2.isBlack(x,y)) == false) {
+					merge.setWhite(x,y);
+				} else {
+					merge.setBlack(x,y);
+				}
+			}
+		}
+		return merge;
+	}
 	
-	
-	
+	static void testMerge() {
+		BinaryImage img1 = randomImage(200, 100);
+		BinaryImage img2 = randomImage(200, 200);
+		
+		System.out.println("Images Created, Merging");
+		merge(img1, img2);
+		System.out.println("Images Merged");
+	}
 	
 	//	h) Pintar um círculo de pontos aleatórios numa imagem dada, dados o centro do círculo e o raio.
 	//	Neste exemplo, foi pintado um círculo com centro no ponto (50, 100) e raio 20 numa imagem preta de dimensões 100x200.
 	
-	
-	
-	
-	
-	
+	static BinaryImage circle(int x, int y, int raio) {
+		
+		BinaryImage circle = new BinaryImage(400,400);
+		
+		for (int w = 0; w < circle.getWidth(); w++)
+			for (int h = 0; h < circle.getHeight(); h++)
+				if (Math.sqrt( (x-w)*(x-w)+(y-h)*(y-h)) < raio) // (x-x0)^2 + (y-y0)^2 = r
+					if (Math.random() < 0.5)
+						circle.setWhite(w,h);
+		
+		return circle;
+	}
 }
