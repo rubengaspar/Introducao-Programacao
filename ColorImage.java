0ed6caff3c9da2b48015dee881158a2979e40a4b
rubengaspar <46534187+rubengaspar@users.noreply.github.com>
2021-11-24 15:24:44 +0000

    Update ColorImage.java

--- ColorImage.java
+++ ColorImage.java
@@ -37,33 +37,128 @@ class ColorImage {
 	// 1. Inverter as cores de uma imagem. Deverá ser utilizada a função desenvolvida 
 	// nos exercícios sobre cores
 	
-	Color inverted(Color c) {    
-		Color invColor = new Color(255 - c.getR(), 255 - c.getG(), 255 - c.getB());
-	return invColor;
+	ColorImage invertColor(ColorImage img) {    
+		
+		ColorImage newImg = new ColorImage(img.getWidth(),img.getHeight());
+		
+		for (int x = 0; x != img.getWidth(); x++) {
+			for (int y = 0; y != img.getHeight(); y++) {
+				Color invColor = new Color( 255 - img.getColor(x, y).getR(), 
+											255 - img.getColor(x,y).getG(), 
+											255 - img.getColor(x,y).getB());
+				newImg.setColor(x,y,invColor);
+			}
+		}
+		
+	return newImg;
 	}
 	
 	// 2. Transformar a imagem, tornando-a mais clara de acordo com um valor dado. 
 	// Um valor positivo torna a imagem mais clara, um valor negativo torna a 
 	// imagem mais escura. Deverá ser utilizada a função desenvolvida nos exercícios sobre cores.
 	
-	Color changeBrightness (Color c, int delta) {    
+	ColorImage changeBrightness (ColorImage img, int delta) {    
 		
-		int min = 0;
-		int max = 255;
+		ColorImage newImg = new ColorImage(img.getWidth(), img.getHeight());
+	
+		for (int x = 0; x != img.getWidth(); x++) {
+			for (int y = 0; y != img.getHeight(); y++) {
+				if ( ((img.getColor(x,y).getR() + delta) < 0) || 
+					 ((img.getColor(x,y).getG() + delta) < 0) || 
+					 ((img.getColor(x,y).getB() + delta) < 0) ||
+					 ((img.getColor(x,y).getR() + delta) > 255) || 
+					 ((img.getColor(x,y).getG() + delta) > 255) || 
+					 ((img.getColor(x,y).getB() + delta) > 255)) {
+					
+					Color newColor1 = img.getColor(x, y);
+					newImg.setColor(x, y, newColor1);
 				
-		if ( ((c.getR() + delta) < min) || 
-			((c.getG() + delta) < min) || 
-			((c.getB() + delta) < min) ||
-			((c.getR() + delta) > max) || 
-			((c.getG() + delta) > max) || 
-			((c.getB() + delta) > max))  {
-			
-			System.out.println("Luminance Change too high");
-			return c;
-		
-		} else {
-			Color newColor = new Color(c.getR() + delta, c.getG() + delta, c.getB() + delta);
-			return newColor;
-		}		
+				} else {
+					Color newColor2 = new Color( delta + img.getColor(x,y).getR(), 
+												delta + img.getColor(x,y).getG(), 
+												delta + img.getColor(x,y).getB());
+					newImg.setColor(x, y, newColor2);
+					
+				}		
+			}	
+		}
+		return newImg;
 	}
+	
+	// 3. Espelhar a imagem horizontalmente.
+	ColorImage horizontal(ColorImage img) {
+		
+		ColorImage newImg = new ColorImage(img.getWidth(),img.getHeight());
+		
+		for (int x = 0; x != img.getWidth(); x++) {
+			for (int y = 0; y != img.getHeight(); y++) {
+				newImg.setColor(x, y, img.getColor(img.getWidth()-1-x, y));
+			}
+		}
+		return newImg;
+		
+	}
+	
+	// 4. Modificar uma imagem, colando-lhe outra imagem (paste) em determinado ponto.
+	
+	ColorImage paste(ColorImage img1, ColorImage img2, int w, int h) {
+		
+		ColorImage newImg = new ColorImage(img1.getWidth(), img1.getHeight());
+		
+		for (int x = 0; x != img2.getWidth(); x++) {
+			for (int y = 0; y != img2.getHeight();y++) {
+				img1.setColor(x+w, y+h, img2.getColor(x, y));
+			}
+		}
+		
+		return newImg;
+	}
+	
+	// 5. Devolver uma versão da imagem a preto e branco (obtendo uma nova imagem). 
+	// Para tal, obtenha o valor da luminância da cor de cada píxel. 
+	// Se a luminância for menor que 128, o píxel nessa posição na imagem a preto e branco deve ser preto, caso contrário deve ser branco.
+
+	ColorImage bw(ColorImage img) {
+		
+		ColorImage newImg = new ColorImage(img.getWidth(),img.getHeight());
+		
+		for (int x = 0; x != img.getWidth(); x++) {
+			for (int y = 0; y != img.getHeight(); y++) {
+				if (img.getColor(x, y).getLuminance() < 128)
+					newImg.setColor(x, y, Color.Black);
+				else
+					newImg.setColor(x, y, Color.White);
+			}
+		}
+			
+		return newImg;
+	}
+	
+	// 6. Devolver uma copia de uma imgem
+	ColorImage copy(ColorImage img) {
+		
+		ColorImage copy = new ColorImage(img.getWidth(),img.getHeight());
+		
+		for (int x = 0; x != img.getWidth(); x++) {
+			for (int y = 0; y != img.getHeight(); y++) {
+				copy.setColor(x, y, img.getColor(x, y));
+			}
+		}
+		return copy;
+	}
+	
+	// 7. Devolver uma seleção rectangular da imagem, dados o canto superior esquerdo e o canto inferior direito.
+	ColorImage selection(ColorImage img, int startx, int starty, int endx, int endy) {
+		
+		ColorImage selection = new ColorImage(endx-startx,endy-starty);
+		
+		for (int x = startx; x != endx; x++) {
+			for (int y = starty; y != endy; y++) {
+				selection.setColor(x-startx, y-starty, img.getColor(x, y));
+			}
+		}
+		return selection;
+	}
+	
+	
 }
